319e787ce92bd4b2da6e674f86be1962a8325889
Main Solutions <juampi_fs@hotmail.com>
2014-11-10 14:38:50 -0300

    Commit inicial

---
+++ app/controllers/offers_controller.rb
@@ -0,0 +1,22 @@
+class OffersController < ApplicationController
+  def new
+  end
+
+  def index
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
