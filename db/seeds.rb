4c4967eb10cbcf0b71e362767cfc4b8e1709854e
Juan Fernandez Sosa <juampi@MacBook-Pro-de-Juan.local>
2015-10-15 18:04:37 -0300

    seed

---
+++ db/seeds.rb
@@ -0,0 +1,2 @@
+User.create(nombre: "admin", apellido: "admin", username:"admin", email:"admin@ing.com", password:"admin")  
+User.create(nombre: "user",  apellido: "user", username:"user", email:"user@ing.com", password:"user")
