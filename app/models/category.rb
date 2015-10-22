7e851eab9e2b85ad290ae8f8963b68eb81f272ad
Juan Fernandez Sosa <juampi@MacBook-Pro-de-Juan.local>
2015-10-22 00:01:50 -0300

    cambios

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,9 +5,9 @@ class << self
 def primerasdosnovacias
 	cant=0
 	aux =0
-	cate=["",""]
+	cate=[]
 	categorias = Category.where(eliminado: false)
-	while (categorias.length >= aux && cant < 2) do
+	while (categorias.length > aux && cant < 2) do
 		if (Article.where(category_id: categorias[aux].id).length != 0)
 			cate[cant]=categorias[aux]
 			cant += 1
